9fb7b6df6491e94ec4bf751c23391a6821f5f828
Andrew <ajg2440@rit.edu>
2012-09-16 13:22:01 -0400

    finished updating createTiles() Needs more testing

--- src/Map.java
+++ src/Map.java
@@ -13,17 +13,82 @@ public class Map {
 	{
 		ArrayList<Tile> temp = new ArrayList<Tile>();
 		int[] setup = {4, 3, 4, 4, 4, 3, 1};
+		
 		for(int i = 0; i < 3; i++)
 		{
 			int type = (int)Math.random()*7;
-			if(setup[type] > 0)
+
+			while(setup[type] <= 0)
 			{
-				temp.add(new Tile(type));
-				setup[type]--;
-			}s
-			
+				type = (int)Math.random()*7;					
+			}
+			temp.add(new Tile(type));
+			setup[type]--;
 		}
-			
+		tiles.add(temp);
+		temp = new ArrayList<Tile>();
 		
+		for(int i = 0; i < 4; i++)
+		{
+			int type = (int)Math.random()*7;
+
+			while(setup[type] <= 0)
+			{
+				type = (int)Math.random()*7;					
+			}
+			temp.add(new Tile(type));
+			setup[type]--;
+		}
+		tiles.add(temp);
+		temp = new ArrayList<Tile>();
+		
+		for(int i = 0; i < 5; i++)
+		{
+			int type = (int)Math.random()*7;
+
+			while(setup[type] <= 0)
+			{
+				type = (int)Math.random()*7;					
+			}
+			temp.add(new Tile(type));
+			setup[type]--;
+		}
+		tiles.add(temp);
+		temp = new ArrayList<Tile>();
+		
+		for(int i = 0; i < 4; i++)
+		{
+			int type = (int)Math.random()*7;
+
+			while(setup[type] <= 0)
+			{
+				type = (int)Math.random()*7;					
+			}
+			temp.add(new Tile(type));
+			setup[type]--;
+		}
+		tiles.add(temp);
+		temp = new ArrayList<Tile>();
+		
+		for(int i = 0; i < 3; i++)
+		{
+			int type = (int)Math.random()*7;
+
+			while(setup[type] <= 0)
+			{
+				type = (int)Math.random()*7;					
+			}
+			temp.add(new Tile(type));
+			setup[type]--;
+		}
+		tiles.add(temp);
+		temp = new ArrayList<Tile>();
+	}
+	public String toString()
+	{
+		
+		
+		
+	}
 	}
 }
