c388bf319c798823c7ad55931ec70abafe7a5286
Andrew <ajg2440@rit.edu>
2012-09-16 02:44:16 -0400

    Fleshed out Point class. Still needs more work, but this is a good start

--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,45 @@
 
 //Tips of Hexagons
 public class Point {
+	public enum Occupation
+	{
+		EMPTY,
+		TOWN,
+		CITY
+	}
+	public enum Owner
+	{
+		EMPTY,
+		RED,
+		BLUE,
+		ORANGE,
+		WHITE
+	}
+	
+	boolean hasHarbor;
+	private Occupation occ;
+	private Owner owner;
 	
 	public Point()
 	{
-		
+		owner = Owner.EMPTY;
+		occ = Occupation.EMPTY;
+		hasHarbor = false;
 	}
+	public void setOccupation(Occupation o)
+	{
+		occ = o;
+	}
+	public  Occupation getOccupation()
+	{ return occ; }
+	public void setOwner(Owner ow)
+	{
+		owner = ow;
+	}
+	public void toggleHarbor()
+	{
+		hasHarbor = !hasHarbor;
+	}
+	public boolean getHarborStatus()
+	{ return hasHarbor; }
 }
