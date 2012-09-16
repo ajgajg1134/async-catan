a5229ee01144230b258821195069e0dc3581e4cb
Andrew <ajg2440@rit.edu>
2012-09-16 15:17:09 -0400

    Fixed map creation method. Now properly creates randomly generated map.

--- src/Map.java
+++ src/Map.java
@@ -12,83 +12,92 @@ public class Map {
 	private void createTiles()
 	{
 		ArrayList<Tile> temp = new ArrayList<Tile>();
-		int[] setup = {4, 3, 4, 4, 4, 3, 1};
+		ArrayList<Tile> temp2 = new ArrayList<Tile>();
+		ArrayList<Tile> temp3 = new ArrayList<Tile>();
+		ArrayList<Tile> temp4 = new ArrayList<Tile>();
+		ArrayList<Tile> temp5 = new ArrayList<Tile>();
+		
+		int[] setup = {4, 3, 4, 4, 3, 1};
 		
 		for(int i = 0; i < 3; i++)
 		{
-			int type = (int)Math.random()*7;
+			int type = (int)(Math.random()*6);
 
 			while(setup[type] <= 0)
 			{
-				type = (int)Math.random()*7;					
+				type = (int)(Math.random()*6);					
 			}
 			temp.add(new Tile(type));
 			setup[type]--;
 		}
+
 		tiles.add(temp);
-		temp = new ArrayList<Tile>();
-		
+
 		for(int i = 0; i < 4; i++)
 		{
-			int type = (int)Math.random()*7;
+			int type = (int)(Math.random()*6);
 
 			while(setup[type] <= 0)
 			{
-				type = (int)Math.random()*7;					
+				type = (int)(Math.random()*6);
 			}
-			temp.add(new Tile(type));
+			temp2.add(new Tile(type));
 			setup[type]--;
 		}
-		tiles.add(temp);
-		temp = new ArrayList<Tile>();
-		
+
+		tiles.add(temp2);
 		for(int i = 0; i < 5; i++)
 		{
-			int type = (int)Math.random()*7;
+			int type = (int)(Math.random()*6);
 
 			while(setup[type] <= 0)
 			{
-				type = (int)Math.random()*7;					
+				type = (int)(Math.random()*6);					
 			}
-			temp.add(new Tile(type));
+			temp3.add(new Tile(type));
 			setup[type]--;
 		}
-		tiles.add(temp);
-		temp = new ArrayList<Tile>();
-		
+		tiles.add(temp3);
+
 		for(int i = 0; i < 4; i++)
 		{
-			int type = (int)Math.random()*7;
+			int type = (int)(Math.random()*6);
 
 			while(setup[type] <= 0)
 			{
-				type = (int)Math.random()*7;					
+				type = (int)(Math.random()*6);					
 			}
-			temp.add(new Tile(type));
+			temp4.add(new Tile(type));
 			setup[type]--;
 		}
-		tiles.add(temp);
-		temp = new ArrayList<Tile>();
-		
+		tiles.add(temp4);
+
 		for(int i = 0; i < 3; i++)
 		{
-			int type = (int)Math.random()*7;
+			int type = (int)(Math.random()*6);
 
 			while(setup[type] <= 0)
 			{
-				type = (int)Math.random()*7;					
+				type = (int)(Math.random()*6);					
 			}
-			temp.add(new Tile(type));
+			temp5.add(new Tile(type));
 			setup[type]--;
 		}
-		tiles.add(temp);
-		temp = new ArrayList<Tile>();
+		tiles.add(temp5);
+
 	}
 	public String toString()
 	{
+		String s = "";
 		
-		
-		
-	}
+		for(ArrayList<Tile> arr : tiles)
+		{
+			for(Tile t : arr)
+			{
+				s += " " + t.toString();
+			}
+			s += "\n";
+		}
+		return s;	
 	}
 }
