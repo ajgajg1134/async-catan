9846f59961c94ee788230665842060dd53355603
Andrew <ajg2440@rit.edu>
2012-09-17 09:36:54 -0400

    Fixed null pointer exception error. Also added error checking for adding too many tile associations to a single point.

--- src/Point.java
+++ src/Point.java
@@ -16,6 +16,8 @@ public class Point {
 		WHITE
 	}
 	
+	Tile[] adjTiles = new Tile[3];
+	
 	boolean hasHarbor;
 	private Occupation occ;
 	private Owner owner;
@@ -25,10 +27,31 @@ public class Point {
 		owner = Owner.EMPTY;
 		occ = Occupation.EMPTY;
 		hasHarbor = false;
+		for(int i = 0; i < 3; i++)
+			adjTiles[i] = null;
+	}
+	public Point(Tile t0, Tile t1, Tile t2)
+	{
+		super();
+		adjTiles[0] = t0;
+		adjTiles[1] = t1;
+		adjTiles[2] = t2;
 	}
 	public void setOccupation(Occupation o)
 	{
 		occ = o;
+	}
+	public void addAdjTile(Tile t)
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			if(adjTiles[i] == null)
+			{
+				adjTiles[i] = t;
+				return;
+			}
+		}		
+		System.err.print("ERROR: Adding tile to full point failed: " + this.toString());
 	}
 	public  Occupation getOccupation()
 	{ return occ; }
@@ -42,4 +65,14 @@ public class Point {
 	}
 	public boolean getHarborStatus()
 	{ return hasHarbor; }
+	public String toString()
+	{
+		String s = "";
+		for(Tile t: adjTiles)
+		{
+			if(t != null)
+				s += t.toString() + "\n";
+		}
+		return s;
+	}
 }
