3e12b18fdacfbc9ab5ca861339c8a795233a9fd8
Andrew <ajg2440@rit.edu>
2012-09-17 09:37:53 -0400

    Fixed variable use in createPoints() where temp1 was used in place of temp. Only Bottom and Top rows of points currently function.

--- src/Map.java
+++ src/Map.java
@@ -2,12 +2,76 @@
 import java.util.ArrayList;
 public class Map {
 	ArrayList<ArrayList<Tile>> tiles = new ArrayList<ArrayList<Tile>>();
+	ArrayList<ArrayList<Point>> points = new ArrayList<ArrayList<Point>>();
 	public Map()
 	{
 		
 		
 		createTiles();
+		createPoints();
+	}
+	private void createPoints()
+	{
+		//6 rows
+		//Top and bottom have 7pts
+		//then add 2
+		//middle 2 rows have 11pts
+		ArrayList<Point> temp = new ArrayList<Point>();
+		ArrayList<Point> temp1 = new ArrayList<Point>();
+		ArrayList<Point> temp2 = new ArrayList<Point>();
+		ArrayList<Point> temp3 = new ArrayList<Point>();
+		ArrayList<Point> temp4 = new ArrayList<Point>();
+		ArrayList<Point> temp5 = new ArrayList<Point>();
+		//Fills temps with empty points
+		for(int i = 0; i < 7; i++)
+		{
+			temp.add(new Point());
+			temp1.add(new Point());
+			temp2.add(new Point());
+			temp3.add(new Point());
+			temp4.add(new Point());
+			temp5.add(new Point());
+		}
+		for(int i = 0; i < 2; i++)
+		{
+			for(int j = 0; j < 2; j++)
+			{			
+				temp2.add(new Point());
+				temp3.add(new Point());
+			}
+			temp1.add(new Point());
+			temp4.add(new Point());
+		}
+		//end fill
+		//start associating points with tiles
+		//Top and Bottom row
+		for(int i = 0; i < 7; i++)
+		{
+			if(i < 3)
+			{
+				temp.get(i).addAdjTile(tiles.get(0).get(0));
+				temp5.get(i).addAdjTile(tiles.get(4).get(0));
+			}
+			if(i > 1 && i < 5)
+			{
+				temp.get(i).addAdjTile(tiles.get(0).get(1));
+				temp5.get(i).addAdjTile(tiles.get(4).get(1));
+			}
+			if(i > 3)
+			{
+				temp.get(i).addAdjTile(tiles.get(0).get(2));
+				temp5.get(i).addAdjTile(tiles.get(4).get(2));
+			}
+		}
 		
+		
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		Map test = new Map();
+		System.out.print(test.toString());
 	}
 	private void createTiles()
 	{
@@ -17,6 +81,7 @@ public class Map {
 		ArrayList<Tile> temp4 = new ArrayList<Tile>();
 		ArrayList<Tile> temp5 = new ArrayList<Tile>();
 		
+		//This array relates to enum in Tile class
 		int[] setup = {4, 3, 4, 4, 3, 1};
 		
 		for(int i = 0; i < 3; i++)
