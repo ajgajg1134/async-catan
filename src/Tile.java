97ea8cf983c91119f3ed6d75b2ba6b99456b2dde
Andrew <ajg2440@rit.edu>
2012-09-18 00:43:47 -0400

    updated toString() to include token value information

--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,3 @@
-
-
 public class Tile {
 	public enum tileType
 	{
@@ -15,18 +13,22 @@ public class Tile {
 	public Tile(int tp)
 	{
 		type = tileType.values()[tp];
-		
 	}
 	public Tile(tileType tp, int tk)
 	{
 		type = tp;
 		token = tk;
-	
 	}
 	public tileType getType()
 	{ return type; }
+	public void setToken(int t)
+	{ token = t; }
+	public int getToken()
+	{ return token; }
 	public String toString()
 	{
-		return type.toString();
+		String s = "";
+		s += type.toString() + ":" + token;
+		return s;
 	}
 }
